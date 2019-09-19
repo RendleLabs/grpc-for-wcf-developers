4d68257bc9b57e03e9a278434170c89bc6968708
Mark Rendle <mark@rendlelabs.com>
2019-09-19 19:03:24 +0100

    Cleanup FullStockTickerAuth

--- FullStockTickerSample/grpc/FullStockTickerAuth/FullStockTicker/src/TraderSys.FullStockTickerServer/Startup.cs
+++ FullStockTickerSample/grpc/FullStockTickerAuth/FullStockTicker/src/TraderSys.FullStockTickerServer/Startup.cs
@@ -61,29 +61,4 @@ namespace TraderSys.FullStockTickerServer
             });
         }
     }
-
-    public partial class GrpcDecimal
-    {
-        private const decimal NanoFactor = 1_000_000_000;
-        public GrpcDecimal(long units, int nanos)
-        {
-            Units = units;
-            Nanos = nanos;
-        }
-
-        public long Units { get; }
-        public int Nanos { get; }
-
-        public static implicit operator decimal(GrpcDecimal grpcDecimal)
-        {
-            return grpcDecimal.Units + grpcDecimal.Nanos / NanoFactor;
-        }
-
-        public static implicit operator GrpcDecimal(decimal value)
-        {
-            var units = decimal.ToInt64(value);
-            var nanos = decimal.ToInt32((value - units) * NanoFactor);
-            return new GrpcDecimal(units, nanos);
-        }
-    }
 }
